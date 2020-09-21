4438c56e4af616753517c3ecfe6f2d581854e4d1
Tanner York <tannerwyork@gmail.com>
2020-09-21 17:46:09 -0500

    Add mnist api logging assignment

---
+++ Assignments/mnist_api_logging/application.py
@@ -0,0 +1,64 @@
+import werkzeug
+werkzeug.cached_property = werkzeug.utils.cached_property
+import tensorflow as tf
+from keras.models import model_from_json
+from PIL import Image
+from werkzeug.datastructures import FileStorage
+import numpy as np
+from flask import Flask, request, jsonify
+from flask_restplus import Api, Resource, fields
+from keras.models import load_model
+from keras.preprocessing.image import img_to_array
+from datetime import date
+import pyrebase
+
+# Setup Firebase App
+from config import firebaseConfig
+firebase = pyrebase.initialize_app(firebaseConfig)
+db = firebase.database()
+
+# Setup Flask App and API
+application = app = Flask(__name__)
+api = Api(app, version='1.0', title='MNIST Classification',
+          description='CNN for MNIST')
+name_space = api.namespace('Make_School', description='Methods')
+
+single_parser = api.parser()
+single_parser.add_argument('file', location='files',
+                           type=FileStorage, required=True)
+
+model = load_model('mnist_cnn.h5')
+graph = tf.get_default_graph()
+
+
+@name_space.route('/prediction')
+class CNNPrediction(Resource):
+    @api.doc(parser=single_parser, description='Upload an MNIST image')
+    def post(self):
+        # Load image file from user as a PIL image
+        args = single_parser.parse_args()
+        image_file = args.file
+        img = Image.open(image_file)
+        # Perform data preprocessing in image
+        image_red = img.resize((28, 28))
+        image = img_to_array(image_red)
+        x = image.reshape(1, 28, 28, 1)
+        x = x/255
+        # Make and return the prediction to the user
+        with graph.as_default():
+            out = model.predict(x)
+
+        # Log interation in firebase
+        data = {
+            "date": date.today().strftime('%Y-%m-%d'), 
+            "image_name": image_file.filename, 
+            "prediction": str(np.argmax(out[0]))
+        }
+        db.child("logs").push(data)
+
+        return {'prediction': str(np.argmax(out[0]))}
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
---
+++ Assignments/mnist_api_logging/config.example.py
@@ -0,0 +1,6 @@
+firebaseConfig = {
+  "apiKey": "apiKey",
+  "authDomain": "projectId.firebaseapp.com",
+  "databaseURL": "https://databaseName.firebaseio.com",
+  "storageBucket": "projectId.appspot.com"
+}
